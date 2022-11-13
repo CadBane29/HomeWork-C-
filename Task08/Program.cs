0fe9ba3521abca9d4d19f422c8ee77d57d7ace98
CadBane29 <115402030+CadBane29@users.noreply.github.com>
2022-11-14 02:12:34 +0300

    add Task08 While 1 to N

---
+++ Task08/Program.cs
@@ -0,0 +1,25 @@
+﻿// Задача 8: Напишите программу, которая 
+// 1. на вход принимает число (N), 
+// 2. а на выходе показывает все чётные числа от 1 до N. //while
+
+// 5 -> 2, 4
+// 8 -> 2, 4, 6, 8
+
+Console.WriteLine("Введите четное число N.");
+int N = Convert.ToInt32(Console.ReadLine());
+int count = 0;
+while (N > 2)
+{
+    if (N % 2 == 0)
+    {
+        Console.Write($"{N}, ");
+        N = N - 2;
+        count++;
+    }
+    else
+    {
+        Console.WriteLine("Вы ввели неккоректное число: ");
+        break;
+    }
+}
+Console.Write(N);
