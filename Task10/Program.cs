61a6275c9d6776a6e133a6cfabdd05e52a066dfb
CadBane29 <115402030+CadBane29@users.noreply.github.com>
2022-11-16 01:56:38 +0300

    add Task10 One Of Them number

---
+++ Task10/Program.cs
@@ -0,0 +1,20 @@
+﻿// Задача 10: Напишите программу, которая 
+// 1. принимает на вход трёхзначное число и на 
+// 2. выходе показывает вторую цифру этого числа. 
+// --Выполнить с помощью числовых операций (целочисленное деление, остаток от деления).
+
+// 456 -> 5
+// 782 -> 8
+// 918 -> 1
+
+Console.WriteLine("Пользователь, введите трёхзначное число: ");
+int secondNumber = Convert.ToInt32(Console.ReadLine());
+
+int OneOfThem (int fullNumber)
+{
+    int result = fullNumber % 100 / 10;
+    return result;
+}
+int oneOfThem = OneOfThem(secondNumber);
+
+Console.WriteLine($"Вторая цифра вашего числа: {oneOfThem}");
