100a0d5b957d5c066aa9f3f92e5c94ea46f4a74e
CadBane29 <115402030+CadBane29@users.noreply.github.com>
2022-11-18 23:09:15 +0300

    add Additional solution to Task15

--- Task15/Program.cs
+++ Task15/Program.cs
@@ -8,10 +8,23 @@
 
 
 
+// Console.WriteLine("Пользователь, введите день недели: ");
+// int dayWeek = Convert.ToInt32(Console.ReadLine());
+
+// if (dayWeek == 6 || dayWeek == 7) Console.Write("Сегодня выходной, расслабляйтесь...");
+// else if (dayWeek < 1 || dayWeek > 7) Console.Write("Введите другое число: от 1 до 7.");
+// else Console.Write("Боюсь сегодня вам необходимо пойти на работу.");
+
 Console.WriteLine("Пользователь, введите день недели: ");
 int dayWeek = Convert.ToInt32(Console.ReadLine());
 
-if (dayWeek == 6 || dayWeek == 7) Console.Write("Сегодня выходной, расслабляйтесь...");
-else if (dayWeek < 1 || dayWeek > 7) Console.Write("Введите другое число, от 1 до 7.");
-else Console.Write("Боюсь сегодня вам необходимо пойти на работу.");
+int DayOff (int day)
+{
+    return day;
+}
+int daysOff = DayOff(dayWeek);
+string result = daysOff == 6 || daysOff == 7 ? "Сегодня выходной, отдохните в свое удовольствие." 
+: daysOff > 0 && daysOff < 6 ? "Похоже сегодня вам необходимо пойти на работу."
+: "Введите другое число: от 1 до 7.";
 
+Console.WriteLine(result);
