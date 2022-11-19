9369f7dd60ecc7e77043640450247e5e420a1f79
CadBane29 <115402030+CadBane29@users.noreply.github.com>
2022-11-20 00:56:23 +0300

    add Task23 Dispaying a table of cube numbers

---
+++ Task23/Program.cs
@@ -0,0 +1,50 @@
+﻿// Задача 23
+// Напишите программу, которая 
+// 1. принимает на вход число (N) и 
+// 2. выдаёт таблицу кубов чисел от 1 до N.
+
+// 5 ->
+// 1 | 1
+// 2 | 8
+// 3 | 27
+// 4 | 64
+// 5 | 125
+
+// int numberN = -1;
+// while(numberN < 1)
+// {
+//     Console.Write("Пользователь, введите число от одного до N: ");
+//     numberN = Convert.ToInt32(Console.ReadLine());
+//     if(numberN < 1) Console.WriteLine("Пользователь, введите корректное число!");
+// }
+
+// string NumberN (int numberCube)
+// {
+//     int counter = 1;
+//     string table = "";
+//     while (counter <= numberCube)
+//     {
+//         table = table + $"{counter} | {counter * counter * counter} \n";
+//         counter++;
+//     }
+//     return table;
+// }
+// string cubeNumber = NumberN(numberN);
+// Console.WriteLine(cubeNumber);
+
+int numberN = -1;
+while(numberN < 1)
+{
+    Console.Write("Пользователь, введите число от одного до N: ");
+    numberN = Convert.ToInt32(Console.ReadLine());
+    if(numberN < 1) Console.WriteLine("Пользователь, введите корректное число!");
+}
+
+void NumberN (int numberCube)
+{
+    for(int i = 1; i <= numberCube; i++)
+    {
+        Console.WriteLine($"Таблица кубов от единицы до вашего числа: {i} | {i * i * i}");
+    }
+}
+NumberN(numberN);
