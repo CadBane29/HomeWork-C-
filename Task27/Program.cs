a0cc40a5d9087459d33c7a5bf2ea94b9bcb0d3a9
CadBane29 <115402030+CadBane29@users.noreply.github.com>
2022-11-23 21:59:49 +0300

    add Task27 Sum of the  entered number

---
+++ Task27/Program.cs
@@ -0,0 +1,24 @@
+﻿// Задача 27: Напишите программу, которая 
+// 1. принимает на вход число и 
+// 2. выдаёт сумму цифр в числе.
+
+// 452 -> 11
+// 82 -> 10
+// 9012 -> 12
+
+Console.Write("Пользователь, введите число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+int Sum(int num)
+{
+    int res = 0;
+    while(num != 0)
+    {
+        res = res + num % 10;
+        num = num / 10;
+    }
+    return Math.Abs(res);
+}
+
+int sum = Sum(number);
+Console.WriteLine($"Сумма цифр числа {number} -> {sum}");
