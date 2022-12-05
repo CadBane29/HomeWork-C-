c3b0838e14880034bb470ad4915e3c18e73b3e8a
CadBane29 <115402030+CadBane29@users.noreply.github.com>
2022-12-05 19:35:13 +0300

    add Task47 Martix array with double numbers

---
+++ Task47/Program.cs
@@ -0,0 +1,42 @@
+﻿// Задача 47. Задайте двумерный массив размером m×n, заполненный случайными вещественными числами.
+// m = 3, n = 4.
+// 0,5 7 -2 -0,2
+// 1 -3,3 8 -9,9
+// 8 7,8 -7,1 9
+
+double[,] CreateMatrixRndDouble(int rows, int columns, int min, int max)
+{
+    double[,] matrix = new double[rows, columns];
+    Random rnd = new Random();
+
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            matrix[i, j] = Math.Round(rnd.NextDouble() * (max - min) + min, 1);
+        }
+    }
+    return matrix;
+}
+
+void PrintMatrixDouble(double[,] matrix)
+{
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        Console.Write("[");
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            if (j < matrix.GetLength(1) - 1) Console.Write($"{matrix[i, j],4} | ");
+            else Console.Write($"{matrix[i, j],4} ");
+        }
+        Console.WriteLine("]");
+    }
+}
+
+Console.Write("Пользователь, введите число строк в массиве: ");
+int numberM = Convert.ToInt32(Console.ReadLine());
+Console.Write("Пользователь, введите число столбцов в массиве: ");
+int numberN = Convert.ToInt32(Console.ReadLine());
+
+double[,] matrix = CreateMatrixRndDouble(numberM, numberN, -10, 10);
+PrintMatrixDouble(matrix);
