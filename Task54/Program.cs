306fa63faa80bd19bdbc9ccce3b81fcb57a7add3
CadBane29 <115402030+CadBane29@users.noreply.github.com>
2022-12-09 12:55:47 +0300

    add Task54 sort in descending matrix row

---
+++ Task54/Program.cs
@@ -0,0 +1,65 @@
+﻿// Задача 54: Задайте двумерный массив. Напишите программу, которая 
+// 1. упорядочит по убыванию элементы каждой строки двумерного массива.
+
+// Например, задан массив:
+// 1 4 7 2
+// 5 9 2 3
+// 8 4 2 4
+// В итоге получается вот такой массив:
+// 7 4 2 1
+// 9 5 3 2
+// 8 4 4 2
+
+int[,] CreateMatrixRndInt(int rows, int columns, int min, int max)
+{
+    int[,] matrix = new int[rows, columns];
+    Random rnd = new Random();
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            matrix[i, j] = rnd.Next(min, max + 1);
+        }
+    }
+    return matrix;
+}
+
+void PrintMatrix(int[,] matrix)
+{
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        Console.Write("\n[");
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            Console.Write("{0,4}", matrix[i, j]);
+            Console.Write(j < matrix.GetLength(1) - 1 ? ","
+                         : string.Empty);
+        }
+        Console.Write(" ]\n");
+    }
+}
+
+void SortRowsByMatrix(int[,] matrix)
+{
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            for (int k = 0; k < matrix.GetLength(1); k++)
+            {
+                if (matrix[i, j] > matrix[i, k])
+                {
+                    int max = matrix[i, j];
+                    matrix[i, j] = matrix[i, k];
+                    matrix[i, k] = max;
+                }
+            }
+        }
+    }
+}
+
+int[,] matrix = CreateMatrixRndInt(3, 4, -10, 10);
+PrintMatrix(matrix);
+Console.WriteLine();
+SortRowsByMatrix(matrix);
+PrintMatrix(matrix);
