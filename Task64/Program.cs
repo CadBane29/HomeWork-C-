e02977ccf6c3422ab1746d1fa49fc0b4876898c7
CadBane29 <115402030+CadBane29@users.noreply.github.com>
2022-12-13 01:05:44 +0300

    add Task64 Recursion method for N values

---
+++ Task64/Program.cs
@@ -0,0 +1,26 @@
+﻿// Задача 64: Задайте значение N. 
+// 1. Напишите программу, которая выведет все натуральные числа в промежутке от N до 1. 
+// 2. Выполнить с помощью рекурсии.
+
+// N = 5 -> "5, 4, 3, 2, 1"
+// N = 8 -> "8, 7, 6, 5, 4, 3, 2, 1"
+
+void NaturalNumberRec(int num)
+{
+    if (num != 0)
+    {
+        Console.Write($"{num} ");
+        NaturalNumberRec(num - 1);
+    }
+}
+
+int numbN = -1;
+while (numbN <= 0)
+{
+    Console.Write("Введите натуральное число: ");
+    numbN = Convert.ToInt32(Console.ReadLine());
+    if (numbN <= 0) Console.WriteLine("Введите другое число!");
+}
+
+Console.Write($"N = {numbN} -> ");
+NaturalNumberRec(numbN);
